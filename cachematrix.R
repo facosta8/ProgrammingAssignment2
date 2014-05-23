975fc34f2a30ca1689d5788dc74cbd57ec2598b3
facosta8 <facosta8@users.noreply.github.com>
2014-05-23 01:20:29 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Assignment 2 for the Coursera "R programming" course 
+# Caching the Inverse of a Matrix
+#
+# As indicated, these functions are used to create a matrix object and calculate its inverse - 
+# this value is stored and can be retrieved as long as the matrix does not change.
 
-## Write a short comment describing this function
+# first function, which creates a matrix object and stores its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = numeric()) {
+      # we create a square matrix filled with random numbers of dimensions NxN,
+      # where N = x
+      x <- matrix(runif(x^2),x,x)     
+      m <- NULL                        # we clean the cache
+                               # declare the functions that will be the output
+      set <- function(y) {     # store out matrix  
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinv <- function(solve) m <<- solve
+      getinv <- function() m
+      list(set = set, get = get,       # output is a list of functions
+           setinv = setinv,
+           getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+# this functions looks in the memory to see if we already have the inverse of
+# the matrix. If it's not there, it calculates the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getinv()                     # retrieve data from memory
+      if(!is.null(m)) {                   # if there's data, return that value
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()                     # if there's no data, the solve and 
+      m <- solve(data, ...)               # set that into the memory of the variable.
+      x$setinv(m)
+      m
 }
